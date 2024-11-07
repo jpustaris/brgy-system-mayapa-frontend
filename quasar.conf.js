62edba63338db2324f50400369afb1759fdde6e4
John Paul <johnpaulustaris@gmail.com>
2024-11-08 04:11:56 +0800

    added config in quasarconfig file

--- quasar.conf.js
+++ quasar.conf.js
@@ -46,7 +46,10 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      env: envparser(ctx),
+      env: {
+        API_URL: JSON.stringify(process.env.API_URL),  // Important: Wrap with JSON.stringify()
+        OTHER_VAR: JSON.stringify(process.env.OTHER_VAR)
+      },
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       publicPath: ctx.prod ? '/' : '',
