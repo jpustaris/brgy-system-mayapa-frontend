238863edf3b6750834ec21b7acfb5874f4f27c02
John Paul Ustaris <johnpaulustaris@gmail.com>
2024-11-13 15:43:47 +0800

    revert to pre-deployment changes

--- quasar.conf.js
+++ quasar.conf.js
@@ -46,10 +46,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      env: {
-        API_URL: JSON.stringify(process.env.API_URL),  // Important: Wrap with JSON.stringify()
-        OTHER_VAR: JSON.stringify(process.env.OTHER_VAR)
-      },
+      env: envparser(ctx),
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       publicPath: ctx.prod ? '/' : '',
